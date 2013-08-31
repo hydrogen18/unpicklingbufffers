3548ba3a390ef131a7791eaba94060f0b8c9ea06
Eric Urban <hydrogen18@gmail.com>
2013-08-31 18:37:04 -0400

    updated with better benchmark approach

--- plot
+++ plot
@@ -1,16 +1,20 @@
 set terminal png size 1024,768
 set key left top Left title 'Legend' box 3
 
+set xlabel 'Pickled Dictionary size'
+set ylabel 'Seconds To Unpickle'
+set title 'Unpickling from a buffer(lower is better)'
+
 set output 'linear.png'
-plot 'dictOfSize.data' index 0 using 2:3 title 'cPickle cStringIO' with lines, \
- 'dictOfSize.data' index 1 using 2:3 title 'cPickle StringIO'  with lines, \
- 'dictOfSize.data' index 2 using 2:3 title 'pickle cStringIO' with lines, \
- 'dictOfSize.data' index 3 using 2:3 title 'pickle StringIO' with lines;
+plot 'dictOfSize.data' index 0 using 2:($3/$1) title 'cPickle cStringIO' with linespoints, \
+ 'dictOfSize.data' index 1 using 2:($3/$1) title 'pickle cStringIO'  with linespoints, \
+ 'dictOfSize.data' index 2 using 2:($3/$1) title 'cPickle StringIO' with linespoints, \
+ 'dictOfSize.data' index 3 using 2:($3/$1) title 'pickle StringIO' with linespoints;
 
 set logscale y
 set key right bottom Left title 'Legend' box 3
 set output 'log.png'
-plot 'dictOfSize.data' index 0 using 2:3 title 'cPickle cStringIO' with lines, \
- 'dictOfSize.data' index 1 using 2:3 title 'cPickle StringIO'  with lines, \
- 'dictOfSize.data' index 2 using 2:3 title 'pickle cStringIO' with lines, \
- 'dictOfSize.data' index 3 using 2:3 title 'pickle StringIO' with lines;
+plot 'dictOfSize.data' index 0 using 2:($3/$1) title 'cPickle cStringIO' with linespoints, \
+ 'dictOfSize.data' index 1 using 2:($3/$1) title 'pickle cStringIO'  with linespoints, \
+ 'dictOfSize.data' index 2 using 2:($3/$1) title 'cPickle StringIO' with linespoints, \
+ 'dictOfSize.data' index 3 using 2:($3/$1) title 'pickle StringIO' with linespoints;
--- unpicklingBuffers.py
+++ unpicklingBuffers.py
@@ -29,34 +29,41 @@ picklers = [pickle,cPickle]
 sios = [StringIO,cStringIO]            
 
 def benchmark(obj):
-    minTime = 0.1
-    iterations = 1
+    minTime = 10.0
     modules = list(itertools.product(picklers,sios))
     modules.reverse()
     
-    while True:
-        results = {}
+    
+    results = {}
+    for p,sio in modules:
+        results['%s %s' % (p.__name__,sio.__name__,)] = []
+        
+    for l in range(4,16):
+        l = 2**l
+        l -= 1
         for p,sio in modules:
-            results['%s %s' % (p.__name__,sio.__name__,)] = []
+            iterations = 1
             
-        for l in range(4,16):
-            l = 2**l
-            l -= 1
-            for p,sio in modules:
+            while True:
                 t = timeit.Timer(stmt='bmark()',setup='gc.enable();import unpicklingBuffers as m; bmark = m.%s(m.%s,m.%s,%i)' % (obj.__name__,p.__name__,sio.__name__,l,))
                 runtime = t.timeit(iterations)
 
                 if runtime < minTime:
-                    iterations *= minTime/runtime
-                    iterations *= 1.01
-                    iterations = int(iterations)
-                    break
+                    newIterations = iterations
+                    newIterations *= minTime/runtime
+                    newIterations *= 1.05
+                    newIterations = int(newIterations)
+                    
+                    if newIterations <= iterations:
+                        iterations *= 2
+                    else:
+                        iterations = newIterations
+                    continue
                 
                 results['%s %s' % (p.__name__,sio.__name__,)].append((iterations,l,runtime))
                 print '%s %s %i %i %f' % ( p.__name__,sio.__name__, l, iterations, runtime,)
-        else:
-            break
-            
+                break
+        
     with open(obj.__name__ +'.data','w') as fout:
         for name,resultset in results.iteritems():
             fout.write('#%s\n' % name)
